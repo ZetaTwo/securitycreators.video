fe15988867c6b355bd63be3abdb4fdc42b7fcf1a
Calle Svensson <calle.svensson@zeta-two.com>
2024-01-23 16:25:11 +0100

    Remove support links in footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,8 +1,6 @@
 <template>
 <footer>
-  Contact: <a href="mailto:contact@securitycreators.video">contact@securitycreators.video</a> - 
-  Support: <a href="https://paypal.me/ZetaTwo" title="Send me money on Paypal"><i class="fab fa-paypal"></i></a>
-  / <a href="https://www.patreon.com/ZetaTwo" title="Support me on Patreon"><i class="fab fa-patreon"></i></a>
+  Contact: <a href="mailto:contact@securitycreators.video">contact@securitycreators.video</a>
 </footer>
 </template>
 
